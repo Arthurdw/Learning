111ed52eb06e08e41c3213d15b7075fcc7b79894
Arthur <arthur.Dewitte@gmail.com>
2019-11-28 21:31:24 +0100

    Test2, final upload lmao

---
+++ School/High School/5'th year/First semester/Program/Revision Test/Opdracht 2/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Opdracht_2 {
+    partial class progrm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.pnlCanvas = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Toegangscode:";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.txtCode.Location = new System.Drawing.Point(13, 34);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(156, 23);
+            this.txtCode.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.btnSubmit.Location = new System.Drawing.Point(13, 63);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(156, 43);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "SUBMIT";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
+            // 
+            // pnlCanvas
+            // 
+            this.pnlCanvas.BackColor = System.Drawing.Color.DarkGray;
+            this.pnlCanvas.Location = new System.Drawing.Point(176, 34);
+            this.pnlCanvas.Name = "pnlCanvas";
+            this.pnlCanvas.Size = new System.Drawing.Size(419, 72);
+            this.pnlCanvas.TabIndex = 3;
+            // 
+            // progrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(618, 122);
+            this.Controls.Add(this.pnlCanvas);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "progrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SESAM UPEN U";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Panel pnlCanvas;
+    }
+}
+
