dfacbafe23b4ff57f2f109a095e24c855ee03966
Arthur <arthur.Dewitte@gmail.com>
2019-11-28 21:30:16 +0100

    Test1

---
+++ School/High School/5'th year/First semester/Program/Revision Test/Opdracht 1/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Opdracht_1 {
+    partial class access {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.btnVerify = new System.Windows.Forms.Button();
+            this.txtRights = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Toegangscode:";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(101, 10);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(100, 20);
+            this.txtCode.TabIndex = 1;
+            // 
+            // btnVerify
+            // 
+            this.btnVerify.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.btnVerify.Location = new System.Drawing.Point(13, 36);
+            this.btnVerify.Name = "btnVerify";
+            this.btnVerify.Size = new System.Drawing.Size(188, 36);
+            this.btnVerify.TabIndex = 2;
+            this.btnVerify.Text = "VALIDEER";
+            this.btnVerify.UseVisualStyleBackColor = true;
+            this.btnVerify.Click += new System.EventHandler(this.BtnVerify_Click);
+            // 
+            // txtRights
+            // 
+            this.txtRights.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.txtRights.Location = new System.Drawing.Point(16, 79);
+            this.txtRights.Name = "txtRights";
+            this.txtRights.Size = new System.Drawing.Size(185, 26);
+            this.txtRights.TabIndex = 3;
+            // 
+            // access
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(212, 115);
+            this.Controls.Add(this.txtRights);
+            this.Controls.Add(this.btnVerify);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "access";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GET ACCESS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Button btnVerify;
+        private System.Windows.Forms.TextBox txtRights;
+    }
+}
+
