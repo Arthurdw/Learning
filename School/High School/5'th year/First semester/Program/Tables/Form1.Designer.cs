9a20fb6eaafc66ef23fdc0f2393fd276cf1465ad
Arthur <arthur.Dewitte@gmail.com>
2019-11-28 21:25:42 +0100

    Add files via upload

---
+++ School/High School/5'th year/First semester/Program/Tables/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Tables {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl_text = new System.Windows.Forms.Label();
+            this.txtNumb = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_text
+            // 
+            this.lbl_text.AutoSize = true;
+            this.lbl_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.lbl_text.Location = new System.Drawing.Point(12, 9);
+            this.lbl_text.Name = "lbl_text";
+            this.lbl_text.Size = new System.Drawing.Size(798, 31);
+            this.lbl_text.TabIndex = 0;
+            this.lbl_text.Text = "From wich number to you want to see the table of multiplications?";
+            // 
+            // txtNumb
+            // 
+            this.txtNumb.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.txtNumb.Location = new System.Drawing.Point(816, 9);
+            this.txtNumb.Name = "txtNumb";
+            this.txtNumb.Size = new System.Drawing.Size(105, 38);
+            this.txtNumb.TabIndex = 1;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.btnCalc.Location = new System.Drawing.Point(12, 53);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(909, 42);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.BtnCalc_Click);
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(12, 101);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.Size = new System.Drawing.Size(909, 337);
+            this.txtResponse.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(930, 450);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtNumb);
+            this.Controls.Add(this.lbl_text);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_text;
+        private System.Windows.Forms.TextBox txtNumb;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtResponse;
+    }
+}
+
