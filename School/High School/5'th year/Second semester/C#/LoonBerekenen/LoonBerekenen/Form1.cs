93c42e91832fcccd4b08a114c8d5df408020200d
Arthur <arthur.Dewitte@gmail.com>
2020-01-26 22:24:34 +0100

    Methods and Functions

---
+++ School/High School/5'th year/Second semester/C#/LoonBerekenen/LoonBerekenen/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LoonBerekenen
+{
+    public partial class Loonberekenen : Form
+    {
+        // Declareer.
+        double uurLoon, aantalUur, basis;
+
+        public Loonberekenen()
+        {
+            InitializeComponent();
+        }
+
+        private void TSMIGewoon_Click(object sender, EventArgs e)
+        {
+            if (Inlezen("Gewoon Loon")) txtLoon.Text = (basis).ToString("#.00-euro");
+        }
+
+        private void TSMINacht_Click(object sender, EventArgs e)
+        {
+            if (Inlezen("Nacht Loon")) txtLoon.Text = (basis * 1.5).ToString("#.00-euro");
+        }
+
+        private void TSMIVerplaatsing_Click(object sender, EventArgs e)
+        {
+            if (Inlezen("Gewoon Loon + Verplaatsing")) txtLoon.Text = (basis + 12).ToString("#.00-euro");
+        }
+
+        private void TSMIWeekend_Click(object sender, EventArgs e)
+        {
+            if (Inlezen("Weekend Loon")) txtLoon.Text = (basis * 2).ToString("#.00-euro");
+        }
+
+        private bool Inlezen(string status)
+        {
+            bool success = true;
+            try
+            {
+                uurLoon = double.Parse(txtUurloon.Text);
+                aantalUur = double.Parse(txtAantal.Text);
+                basis = uurLoon * aantalUur;
+                lblStatus.Text = status;
+            }
+            catch (System.FormatException)
+            {
+                txtLoon.Text = "Fout invoer.";
+                lblStatus.Text = "(status)";
+                success = false;
+            }
+            return success;
+        }
+    }
+}
