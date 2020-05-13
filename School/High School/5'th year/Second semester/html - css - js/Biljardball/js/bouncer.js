6caf351a409d4bade9c04f3a2758783d14a95d7c
Arthur <arthur.Dewitte@gmail.com>
2020-05-13 13:05:15 +0200

    Better this way lol

--- School/High School/5'th year/Second semester/html - css - js/Biljardball/js/bouncer.js
+++ School/High School/5'th year/Second semester/html - css - js/Biljardball/js/bouncer.js
@@ -2,23 +2,15 @@ const frame = document.getElementById("frame");
 const circles = document.getElementsByClassName("circle")[0];
 
 const data = {
-    canvas: {
-        top: frame.offsetTop,
-        bottom: frame.offsetTop + frame.offsetHeight,
-        left: frame.offsetLeft,
-        right: frame.offsetLeft + frame.offsetWidth
-    },
-    animation: {
-        goX: -1,
-        goY: -1
-    }
+    goX: -1,
+    goY: -1
 }
 
 setInterval(update, 5);
 
 function update() {
-    circles.style.top = circles.offsetTop + data.animation.goY;
-    circles.style.left = circles.offsetLeft + data.animation.goX;
-    if (circles.offsetTop <= data.canvas.top || circles.offsetHeight + circles.offsetTop >= data.canvas.bottom) data.animation.goY = -data.animation.goY;
-    if (circles.offsetLeft <= data.canvas.left || circles.offsetLeft + circles.offsetWidth >= data.canvas.right) data.animation.goX = -data.animation.goX;
+    circles.style.top = circles.offsetTop + data.goY;
+    circles.style.left = circles.offsetLeft + data.goX;
+    if (circles.offsetTop <= frame.offsetTop || circles.offsetHeight + circles.offsetTop >= frame.offsetTop + frame.offsetHeight) data.goY = -data.goY;
+    if (circles.offsetLeft <= frame.offsetLeft || circles.offsetLeft + circles.offsetWidth >= frame.offsetLeft + frame.offsetWidth) data.goX = -data.goX;
 }
