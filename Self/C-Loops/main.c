5ae130fe402107fc546c5a4f35415684af8c4822
Arthur <arthur.Dewitte@gmail.com>
2020-05-21 16:38:11 +0200

    Add files via upload

---
+++ Self/C-Loops/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    // int index = 0;
+
+    // Normal While loop:
+    // while (index <= 5) {
+    //     printf("%d\n", index);
+    //     index++;
+    // }
+
+    // do while in C:
+    // do {
+    //     printf("%d\n", index);
+    //     index++;
+    // } while (index <= 5)
+
+    // Normal for loop:
+    for (int i = 0; i <= 5; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
