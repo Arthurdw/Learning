550ea5de6ef436c2d677ec94b00823848c3fff2a
Arthur <arthur.Dewitte@gmail.com>
2020-05-21 09:56:20 +0200

    Add files via upload

---
+++ Self/C-HelloWorld/cmake-build-debug/CMakeFiles/Basic_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Basic_C.exe"
+  "Basic_C.exe.manifest"
+  "Basic_C.lib"
+  "Basic_C.pdb"
+  "CMakeFiles/Basic_C.dir/main.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Basic_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
