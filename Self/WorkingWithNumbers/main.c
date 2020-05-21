2ab68732cbb041a783af9e8126baaf003d25c545
Arthur <arthur.Dewitte@gmail.com>
2020-05-21 12:21:55 +0200

    Add files via upload

---
+++ Self/WorkingWithNumbers/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    // Some basic math operations:
+    printf("%f\n", 1.2 + 2.3);
+    printf("%f\n", 1.2 - 2.3);
+    printf("%f\n", 1.2 * 2.3);
+    printf("%f\n", 1.2 / 2.3);
+
+    // Working with functions:
+    printf("%d\n", (int) pow(10, 2));
+    printf("%d\n", (int) sqrt(36));
+    printf("%d\n", (int) ceil(123.123));
+    printf("%d", (int) floor(123.123));
+
+    return 0;
+}
