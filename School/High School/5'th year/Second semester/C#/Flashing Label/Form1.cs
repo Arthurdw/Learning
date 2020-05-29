af6edc2cc91eb1dda6bda36623805d1e7520fb59
Arthur <arthur.Dewitte@gmail.com>
2020-05-29 11:21:40 +0200

    Add files via upload

---
+++ School/High School/5'th year/Second semester/C#/Flashing Label/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Flashing_Label
+{
+    public partial class Form1 : Form
+    {
+        public Form1() { InitializeComponent(); }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rn = new Random();
+            Color ForeColor = Color.FromArgb(rn.Next(0, 255), rn.Next(0, 255), rn.Next(0, 255), rn.Next(0, 255));
+            label1.ForeColor = button1.ForeColor = textBox1.ForeColor = ForeColor;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer1.Enabled = true;
+        }
+    }
+}
---
+++ School/High School/5'th year/Second semester/C#/FlashyAlert/AlertDisplay.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace FlashyAlert
+{
+    partial class AlertDisplay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ptbIcon = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ptbClose = new System.Windows.Forms.PictureBox();
+            this.FadeTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbIcon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptbIcon
+            // 
+            this.ptbIcon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ptbIcon.Location = new System.Drawing.Point(12, 12);
+            this.ptbIcon.Name = "ptbIcon";
+            this.ptbIcon.Size = new System.Drawing.Size(100, 100);
+            this.ptbIcon.TabIndex = 0;
+            this.ptbIcon.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(118, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "<Message here>";
+            // 
+            // ptbClose
+            // 
+            this.ptbClose.BackgroundImage = global::FlashyAlert.Properties.Resources.close;
+            this.ptbClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ptbClose.Location = new System.Drawing.Point(310, 12);
+            this.ptbClose.Name = "ptbClose";
+            this.ptbClose.Size = new System.Drawing.Size(30, 30);
+            this.ptbClose.TabIndex = 2;
+            this.ptbClose.TabStop = false;
+            this.ptbClose.Click += new System.EventHandler(this.ptbClose_Click);
+            // 
+            // FadeTimer
+            // 
+            this.FadeTimer.Tick += new System.EventHandler(this.FadeTimer_Tick);
+            // 
+            // AlertDisplay
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(349, 123);
+            this.Controls.Add(this.ptbClose);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ptbIcon);
+            this.Font = new System.Drawing.Font("Open Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AlertDisplay";
+            this.Opacity = 0D;
+            this.Text = "AlertDisplay";
+            this.Load += new System.EventHandler(this.AlertDisplay_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptbIcon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbClose)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptbIcon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox ptbClose;
+        private System.Windows.Forms.Timer FadeTimer;
+    }
+}
