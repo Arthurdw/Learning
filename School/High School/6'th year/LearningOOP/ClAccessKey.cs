8ed67070cca1b803c9fe699fe0d8f79a49feb45c
Arthur <arthur.Dewitte@gmail.com>
2020-10-08 14:07:47 +0200

    Learning OOP at school.

---
+++ School/High School/6'th year/LearningOOP/ClAccessKey.cs
@@ -0,0 +1,26 @@
+﻿// ©Arthurdw | GO-AO
+using System;
+
+namespace LearningOOP
+{
+    internal class ClAccessKey
+    {
+        private readonly string _accessToken;
+        public ClAccessKey(string input)
+        {
+            try
+            {
+                _accessToken = GenerateCode(input);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                _accessToken = "Code generation failed.";
+            }
+        }
+
+        public string GetCode() => _accessToken;
+
+        private string GenerateCode(string input) => 
+            $"{DateTime.Now.Millisecond}{DateTime.Now.Year}{DateTime.Now.Month}{input.Substring(0, 1)}{input.Length}";
+    }
+}
