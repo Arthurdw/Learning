3ed3680f004a4c8a4de77fa614b17cd31bbf9b88
Arthur <arthur.Dewitte@gmail.com>
2020-10-15 11:40:16 +0200

    Add files via upload

---
+++ School/High School/6'th year/useraccounts/CreateHash.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace useraccounts
+{
+    class CreateHash
+    {
+        private byte[] value;
+        public CreateHash(string input, string scrambler)
+        {
+            value = GetUtfReady(input, scrambler);
+        }
+
+        private byte[] GetUtfReady(string input, string scrambler)
+        {
+            char[] chars = input.ToCharArray();
+            Array.Reverse(chars);
+            input += chars.ToString();
+            return new UTF8Encoding().GetBytes(string.Format(scrambler, input));
+        }
+
+        public string getHashSHA512() => Convert.ToBase64String(new SHA512Managed().ComputeHash(value));
+    }
+}
---
+++ School/High School/6'th year/useraccounts/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace UserAccounts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnToCSV = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Naam:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Passwoord:";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(16, 68);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(84, 40);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(106, 68);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(84, 40);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnToCSV
+            // 
+            this.btnToCSV.Location = new System.Drawing.Point(196, 68);
+            this.btnToCSV.Name = "btnToCSV";
+            this.btnToCSV.Size = new System.Drawing.Size(84, 40);
+            this.btnToCSV.TabIndex = 4;
+            this.btnToCSV.Text = "CSV";
+            this.btnToCSV.UseVisualStyleBackColor = true;
+            this.btnToCSV.Click += new System.EventHandler(this.btnToCSV_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(101, 9);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(179, 22);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(101, 37);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(179, 22);
+            this.txtPass.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 127);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnToCSV);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnToCSV;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPass;
+    }
+}
+
