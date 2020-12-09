efd2b9aa1ffa5643473fb3318085fcb34cae3557
Arthur <arthur.Dewitte@gmail.com>
2020-12-09 14:50:18 +0100

    Example DB connector

---
+++ School/High School/6'th year/UsefullClasses/UsefullClasses/DatabaseInteractionHandler.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace UsefullClasses
+{
+    [Serializable]
+    internal class UnableToConnectException : Exception
+    {
+        public string Message { get; }
+
+        public UnableToConnectException(string connectionMessage) : base($"Can not connect to database. Connection string: {connectionMessage}")
+        {
+            Message = connectionMessage;
+        }
+    }
+
+    internal class DatabaseInteractionHandler
+    {
+        public MySqlConnection Connection { get; }
+        public MySqlDataAdapter Adapter { get; }
+
+        public string ConnectionString { get; }
+
+        public DatabaseInteractionHandler(string userName, string password, string database, string ip, string port)
+        {
+            ConnectionString =
+                $"Server={ip};Port={port};SslMode=none;Database={database};Uid={userName};Pwd={password}";
+            Connection = new MySqlConnection(ConnectionString);
+            TestConnection();
+        }
+
+        public void TestConnection()
+        {
+            try
+            {
+                Connection.Open();
+                Connection.Close();
+            }
+            catch (MySqlException)
+            {
+                throw new UnableToConnectException(ConnectionString);
+            }
+        }
+    }
+}
