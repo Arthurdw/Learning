5da3e58ad4740b52347491e024a925a61c21847d
Arthur <arthur.Dewitte@gmail.com>
2021-04-05 14:37:52 +0200

    Add files via upload

---
+++ School/High School/6'th year/SquareBallz/Ballz.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Balletjes
+{
+    internal class Ballz
+    {
+        public Point Location;
+        public readonly int Size;
+        private readonly Brush _br;
+        public int GoX;
+        public int GoY;
+        public int id;
+
+        public static int lastId;
+        public static List<Ballz> All;
+        public static Action collisionDetected;
+
+        public Ballz(Point location, Color color, int size)
+        {
+            this.Location = location;
+            this.Size = size;
+            this._br = new SolidBrush(color);
+            this.id = Ballz.lastId++;
+
+            // We need to manually set the dynamic seed to get more "true" randomness.
+            Random rn = new Random(DateTime.Now.Millisecond + size);
+
+            do
+            {
+                this.GoX = rn.Next(-1, 2);
+                this.GoY = rn.Next(-1, 2);
+            } while (this.GoX == 0 || this.GoY == 0);
+        }
+
+        public void Draw()
+            => GraphicsHandler.graphic.FillRectangle(this._br, this.Location.X, this.Location.Y, this.Size, this.Size);
+
+        public void PerformStep()
+        {
+            this.Location.X += this.GoX;
+            this.Location.Y += this.GoY;
+
+            if (this.Location.Y <= GraphicsHandler.panel.Location.Y ||
+                this.Location.Y + this.Size >= GraphicsHandler.panel.Location.Y + GraphicsHandler.panel.Height)
+                this.GoY = -this.GoY;
+            if (this.Location.X <= GraphicsHandler.panel.Location.X ||
+                this.Location.X + this.Size >= GraphicsHandler.panel.Location.X + GraphicsHandler.panel.Width)
+                this.GoX = -this.GoX;
+
+            this.HandleCollision();
+        }
+
+        private void HandleCollision()
+        {
+            Rectangle current = this.Rectangle();
+            Ballz[] selected = new Ballz[Ballz.All.Count];
+            Ballz.All.CopyTo(selected);
+
+            foreach (Ballz circle in selected)
+            {
+                if (circle.id != this.id && current.IntersectsWith(circle.Rectangle()))
+                {
+                    // Prevent inside looping
+                    // if (!(circle.GoX != this.GoX && circle.GoY != this.GoY)) continue;;
+
+                    // Manually handle all cases... yes I know this is probably the worst way to handle this!
+                    this.GoX = -this.GoX;
+                    this.GoY = -this.GoY;
+
+                    Console.WriteLine(@"collision detected");
+                    Console.WriteLine($@"Circle: {circle.Location.X}-{circle.Location.Y}");
+                    Console.WriteLine($@"Circle: {circle.GoX}-{circle.GoY}");
+                    Console.WriteLine($@"Current: {this.Location.X}-{this.Location.Y}");
+                    Console.WriteLine($@"Current: {this.GoX}-{this.GoY}");
+                    Console.WriteLine(@"------------------");
+
+                    if (Ballz.lastId >= 300) continue;
+                    Ballz.collisionDetected();
+                }
+            }
+        }
+
+        public Rectangle Rectangle() => new Rectangle(this.Location, new Size(this.Size, this.Size));
+    }
+}
---
+++ School/High School/6'th year/SquareBallz/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Balletjes
+{
+    public partial class Form1 : Form
+    {
+        private Random _rn;
+        private double _size;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeProgram();
+        }
+
+        private void InitializeProgram()
+        {
+            Ballz.lastId = 0;
+            Ballz.All = new List<Ballz>();
+            Ballz.collisionDetected = this.SpawnRandomBall;
+            GraphicsHandler.panel = panel;
+            GraphicsHandler.graphic = panel.CreateGraphics();
+            GraphicsHandler.graphic.SmoothingMode = SmoothingMode.AntiAlias;
+
+            // Prevent flickering
+            typeof(Panel).InvokeMember("DoubleBuffered",
+                // Bitwise operation to set properties
+                BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+
+                // Lets bind it to our panel and our form.
+                null, panel, new object[] { true });
+            _rn = new Random();
+            _size = 40;
+        }
+
+        private void AddBall(Point location)
+        {
+            Ballz.All.Add(new Ballz(location, Color.FromArgb(_rn.Next(256), _rn.Next(256), _rn.Next(256)), Convert.ToInt32(_size)));
+            if (_size > 4) _size -= 0.1;
+        }
+
+        private void HandlePaintClick(object sender, MouseEventArgs e)
+        {
+            if (Ballz.All.Count == 0)
+            {
+                AddBall(e.Location);
+                SpawnRandomBall();
+                panel.Invalidate();
+                drawer.Start();
+            }
+        }
+
+        private void PaintPanel(object sender, PaintEventArgs e)
+        {
+            GraphicsHandler.graphic = e.Graphics;
+            GraphicsHandler.graphic.SmoothingMode = SmoothingMode.AntiAlias;
+            foreach (Ballz circle in Ballz.All) circle.Draw();
+        }
+
+        private void drawer_Tick(object sender, EventArgs e)
+        {
+            Ballz[] selected = new Ballz[Ballz.All.Count];
+            Ballz.All.CopyTo(selected);
+            foreach (Ballz circle in selected) circle.PerformStep();
+            panel.Invalidate();
+        }
+
+        private void SpawnRandomBall()
+            => AddBall(new Point(_rn.Next(panel.Width - Convert.ToInt32(_size)), _rn.Next(panel.Height - Convert.ToInt32(_size))));
+    }
+}
