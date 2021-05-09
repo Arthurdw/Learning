fe3126890f18b7ef569fcb96d79feb70ad83d046
Arthur <arthur.Dewitte@gmail.com>
2021-05-09 20:31:55 +0200

    Add files via upload

---
+++ School/High School/6'th year/AutoScreener/ImageRenamer.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace AutoScreener
+{
+    sealed partial class ImageRenamer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChange = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnChange
+            // 
+            this.btnChange.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChange.Location = new System.Drawing.Point(13, 49);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(275, 53);
+            this.btnChange.TabIndex = 0;
+            this.btnChange.Text = "Wijzig";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(13, 13);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(275, 30);
+            this.txtName.TabIndex = 1;
+            // 
+            // ImageRenamer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 114);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnChange);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "ImageRenamer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ImageRenamer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
---
+++ School/High School/6'th year/AutoScreener/ImageUpdater.cs
@@ -0,0 +1,85 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AutoScreener
+{
+    public partial class ImageUpdater : Form
+    {
+        private readonly MySqlHandler _mySqlHandler;
+        private string _selected;
+
+        public ImageUpdater(MySqlHandler mySqlHandler)
+        {
+            InitializeComponent();
+            this._mySqlHandler = mySqlHandler;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string refId = txtRefId.Text.Trim();
+            if (string.IsNullOrEmpty(refId))
+            {
+                MessageBox.Show(@"RefId was given.");
+                return;
+            }
+
+            MySqlCommand cmd = this._mySqlHandler.Prepare(SqlStatements.FetchStatement, ("@refnr", refId));
+
+            this._mySqlHandler._connection.Open();
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                byte[] data = (byte[])rdr[0];
+                if (data == null)
+                {
+                    MessageBox.Show(@"Invalid RefId", "Oops...", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+
+                ptbPreview.Image = new Bitmap(ImageHandler.BytesToImage(data), new Size(ptbPreview.Width, ptbPreview.Height));
+                this._selected = refId;
+                break;
+            }
+            rdr.Close();
+            this._mySqlHandler._connection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this._selected == null)
+            {
+                MessageBox.Show(@"Gelieve eerst een bestand te fetchen!");
+                return;
+            }
+
+            FileDialogHandler fdh = new FileDialogHandler();
+            fdh.Filter = "Image files (*.png, *.jpg, *.jpeg)|*.png;*.jpg;*.jpeg|" + fdh.Filter;
+            string selectedFilePath = fdh.OpenFileSelector();
+
+            if (!File.Exists(selectedFilePath))
+            {
+                MessageBox.Show(@"Je hebt geen bestand geselecteerd!");
+                return;
+            }
+
+            this.UpdateSelectedImage(Image.FromFile(selectedFilePath));
+            MessageBox.Show(@"Successfully updated the image!");
+        }
+
+        private void UpdateSelectedImage(Image img)
+        {
+            MySqlCommand cmd = this._mySqlHandler.Prepare(
+                SqlStatements.UpdateImageStatement,
+                ("@id", this._selected),
+                ("@image", ImageHandler.ImageToBytes(img, ImageFormat.Png)));
+            this._mySqlHandler.Execute(cmd);
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+            => new ImageRenamer(this._mySqlHandler, this._selected).Show();
+    }
+}
