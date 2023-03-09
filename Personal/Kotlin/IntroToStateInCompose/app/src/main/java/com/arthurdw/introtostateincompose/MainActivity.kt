bd24df94f192f2c48e6b70f318403365d1a93081
Arthurdw <dev@arthurdw.com>
2023-03-09 11:22:10 +0100

    :sparkles: Added feature to set the tip percentage

--- Personal/Kotlin/IntroToStateInCompose/app/src/main/java/com/arthurdw/introtostateincompose/MainActivity.kt
+++ Personal/Kotlin/IntroToStateInCompose/app/src/main/java/com/arthurdw/introtostateincompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.arthurdw.introtostateincompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
@@ -47,9 +48,11 @@ fun DefaultPreview() {
 @Composable
 fun TipTimeScreen() {
     var amountInput by remember { mutableStateOf("0") }
+    var tipInput by remember { mutableStateOf("") }
 
+    val tipPercent = tipInput.toDoubleOrNull() ?: 0.0
     val amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount)
+    val tip = calculateTip(amount, tipPercent)
 
     Column(
         modifier = Modifier.padding(32.dp), verticalArrangement = Arrangement.spacedBy(8.dp)
@@ -60,7 +63,15 @@ fun TipTimeScreen() {
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
         Spacer(Modifier.height(16.dp))
-        EditNumberField(value = amountInput, onValueChange = { amountInput = it })
+        EditNumberField(
+            value = amountInput, onValueChange = { amountInput = it }, label = R.string.bill_amount
+        )
+        EditNumberField(
+            value = tipInput,
+            onValueChange = { tipInput = it },
+            label = R.string.how_was_the_service
+        )
+        Spacer(Modifier.height(24.dp))
         Text(
             text = stringResource(R.string.tip_amount, tip),
             modifier = Modifier.align(Alignment.CenterHorizontally),
@@ -71,9 +82,14 @@ fun TipTimeScreen() {
 }
 
 @Composable
-fun EditNumberField(value: String, onValueChange: (String) -> Unit, modifier: Modifier = Modifier) {
+fun EditNumberField(
+    @StringRes label: Int,
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
     TextField(
-        label = { Text(text = stringResource(R.string.cost_of_service)) },
+        label = { Text(text = stringResource(label)) },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         singleLine = true,
         value = value,
@@ -83,8 +99,7 @@ fun EditNumberField(value: String, onValueChange: (String) -> Unit, modifier: Mo
 }
 
 private fun calculateTip(
-    amount: Double,
-    tipPercent: Double = 15.0
+    amount: Double, tipPercent: Double = 15.0
 ): String {
     val tip = tipPercent / 100 * amount
     return NumberFormat.getCurrencyInstance().format(tip)
